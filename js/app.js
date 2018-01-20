b1cba94be648e4887d43907c17e068b749c0577c
yourGitHubusername <yourGitHub@email.com>
2018-01-20 23:22:12 +0100

    Text Banner is fading out over time

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 // Immediately-Invoked Function Expression
 (function(){
 // Angular module created
-  angular
-    .module("philosophySentence", [])
+  //angular
+    var app = angular.module("philosophySentence", [])
+
+    app.directive("scroll", function ($window) {
+        return function(scope, element, attrs) {
+
+            angular.element($window).bind("scroll", function() {
+                if (this.pageYOffset >= 600) {
+                     scope.boolChangeClass = true;
+                     console.log('Scrolled below header.');
+                 } else {
+                     scope.boolChangeClass = false;
+                     console.log('Header is in view.');
+                 }
+                scope.$apply();
+            });
+        };
+    });
 
 })();
